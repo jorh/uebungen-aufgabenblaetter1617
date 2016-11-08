8cce362fd9561b8b85bbe3b3dce0463008053c49
u434581 <u434581@R-011.cadlab.tu-berlin.de>
2016-11-08 17:16:17 +0100

    erste Farmversuche

---
+++ src/uebung4/Cow.java
@@ -0,0 +1,8 @@
+package uebung4;
+
+public class Cow {
+	String name;
+	public Cow(String name){
+		this.name = name;
+	}
+}
---
+++ src/uebung4/Farm.java
@@ -0,0 +1,16 @@
+package uebung4;
+
+public class Farm {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Cow cow1 = new Cow("Peter");
+		Cow cow2 = new Cow("Anneliese");
+		Cow cow3 = new Cow("Olaf");
+		
+		System.out.println("Kuh 2 = "+cow1.name);
+		System.out.println("Kuh 2 = "+cow2.name);
+		System.out.println("Kuh 3 = "+cow3.name);
+	}
+
+}
