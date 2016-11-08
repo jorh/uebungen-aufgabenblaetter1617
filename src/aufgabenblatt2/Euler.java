f658e399fc178eaf17ff4c711f90a8becf699155
u434581 <u434581@R-011.cadlab.tu-berlin.de>
2016-11-08 17:07:03 +0100

    Weiter

--- src/aufgabenblatt2/Euler.java
+++ src/aufgabenblatt2/Euler.java
@@ -4,8 +4,14 @@ public class Euler {
 
 	public static double myExp(int x) {
 		// TODO berechne e^x mittels der auf Aufgabenblatt 2 angegebenen Formel
-		int erg, i;
-		i = 1;
+		int erg = 1;
+		int n = 5;
+		int fak=1;
+		for (int i=1;i<n;i++){
+			fak = fak*i;
+			erg = erg + (x^i/fak);			
+		}
+		System.out.println("Die Potenz von e hoch "+x+" ist "+ erg);
 	}
 	
 	public static double myExpFast(int x){
