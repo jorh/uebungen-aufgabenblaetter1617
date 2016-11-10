c44dffeece90a333efaaebc1722fd8f9a7daa7f1
u434581 <u434581@R-106.cadlab.tu-berlin.de>
2016-11-10 14:16:38 +0100

    Fehleranalyse

--- src/aufgabenblatt2/Euler.java
+++ src/aufgabenblatt2/Euler.java
@@ -4,12 +4,16 @@ public class Euler {
 
 	public static double myExp(int x) {
 		// TODO berechne e^x mittels der auf Aufgabenblatt 2 angegebenen Formel
-		int erg = 1;
+		double erg = 1;
 		int n = 5;
 		int fak=1;
+		int potenz = 1;
 		for (int i=1;i<n;i++){
 			fak = fak*i;
-			erg = erg + (x^i/fak);			
+			potenz = potenz*x;
+			System.out.println("fak: "+fak+" "+potenz);
+			erg = erg + (potenz/fak);			
+			System.out.println(i+": erg: "+erg+" fak: "+fak+" i: "+i);
 		}
 		System.out.println("Die Potenz von e hoch "+x+" ist "+ erg);
 		return erg;
